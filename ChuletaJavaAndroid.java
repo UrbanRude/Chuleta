5b50b2193385fa3549cad52ade24bbbd1bb891ec
Urbano Ceron <urban.rude@gmail.com>
2018-02-08 18:24:24 -0600

    CursoNextU

---
+++ ChuletaJavaAndroid.java
@@ -0,0 +1,59 @@
+## CAMBIAR DE ACTIVITY				
+Intent intent = new Intent(this, // Actividad en la que estamos
+					ActividadCambiar); // Actividad a la que nos vamos a dirigir
+startActivity(intent);
+
+## CREAR UN EVENTO DE CLICK
+public void ClickBoton(View view){
+	// OBTENER UN STRING DE LOS RECURSOS values/strings
+	String cadena = getString(R.string.cadena);
+	elemento.setText(cadena);
+}
+
+## ENVIAR DATOS DE UNA ACTIVIDAD A OTRA CON EL METODO INTENT
+Intent intent = new Intent(this, // Avtividad en la que estamos
+					ActividadCambiar); // Actividad a la que nos vamos a dirigir
+// El metodo putExtra recibe dos elementos
+intent.putExtra("NombreLlave", //Nombre de como lo vamos a recibir
+				VariableEnviar); //Variable a Enviar de tipo String
+startActivity(intent);
+
+## CLICO DE VIDA DE UN ACTIVITY
+@Override
+    protected void onStart() {
+        super.onStart();
+        Log.wtf("Ciclo de Vida","onStart");
+    }
+
+@Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.wtf("Ciclo de Vida","onRestart");
+    }
+
+@Override
+    protected void onResume() {
+        super.onResume();
+        Log.wtf("Ciclo de Vida","onResume");
+    }
+
+@Override
+    protected void onPause() {
+        super.onPause();
+        Log.wtf("Ciclo de Vida","onPause");
+    }
+
+@Override
+    protected void onStop() {
+        super.onStop();
+        Log.wtf("Ciclo de Vida","onStop");
+    }
+
+@Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.wtf("Ciclo de Vida","onDestroy");
+    }
+
+## CONBINACION DE TECLAS
+ALT+INS // PARA INSERTAR METODO 
